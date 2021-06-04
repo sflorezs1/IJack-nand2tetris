0445df294238bbbb60bd1ed4bfed115e81bc013a
Johan <jsruiza@eafit.edu.co>
2021-06-03 19:57:39 -0500

    added new draw functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from lexer import IndentLexer
 
 def main():
     lexer = IndentLexer()
-    with open("Examples/Pong/Vector.ijk", "r") as code:
+    with open("Draw/IDraw.ijk", "r") as code:
         lexer.input(code.read())
         while t := lexer.token():
             print(t)
